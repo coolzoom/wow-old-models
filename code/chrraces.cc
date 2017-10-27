0d541bbc23fd321f30d444fb7bdd07e1e3646d13
cqwrteur <euloanty@live.com>
2017-10-27 07:07:37 -0400

    a

--- code/chrraces.cc
+++ code/chrraces.cc
@@ -10,9 +10,9 @@ int main()
 		db2::chrraces_file cf("chrraces.db2");
 		std::ofstream fout("chrraces.txt",std::ofstream::binary);
 		auto rc(cf.records());
-		rc.at(3).m_flags^=0x80;					//set flag for NE
-		rc.at(10).m_flags^=0x80;					//set flag for draenei
-//		rc.at(9).m_flags^=0x80;					//set flag for blood elf. disable it by default since model files are currently broken.
+//		rc.at(3).m_flags^=0x80;					//set flag for NE
+//		rc.at(10).m_flags^=0x80;					//set flag for draenei
+		rc.at(9).m_flags^=0x80;					//set flag for blood elf. disable it by default since model files are currently broken.
 
 
 		for(std::size_t i(0);i!=rc.size();++i)
