16fa2d091b9ea1c0c2a9d7530dbe789f04c2cf83
cqwrteur <euloanty@live.com>
2017-10-27 00:21:45 -0400

    s

--- code/m2.cc
+++ code/m2.cc
@@ -12,7 +12,15 @@ int main(int argc,char **argv)
 			std::cerr<<"Usage : "<<*argv<<" <m2 filename path>\n";
 			return 1;
 		}
-		m2::m2 p(argv[1]);
+		const m2::m2 p(argv[1]);
+		std::cout<<p<<'\n';
+		decltype(auto) md21(p.get<m2::md21>());
+		auto sp(md21.at(md21.head().bones));
+		std::cout<<sp.size()<<'\n';
+		for(const auto &ele : sp)
+		{
+			std::cout<<ele<<'\n';
+		}
 	}
 	catch(const std::exception& ex)
 	{
